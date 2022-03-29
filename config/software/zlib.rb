0150245fec713092e9277b599819d8d7523a87ac
Kylian Serrania <kylian.serrania@datadoghq.com>
2022-03-29 13:33:52 -0400

    [zlib] Bump to 1.2.12

--- config/software/zlib.rb
+++ config/software/zlib.rb
@@ -15,20 +15,13 @@
 #
 
 name "zlib"
-default_version "1.2.11"
+default_version "1.2.12"
 
-version "1.2.11" do
-  source sha256: "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"
+version "1.2.12" do
+  source sha256: "91844808532e5ce316b3c010929493c0244f3d37593afd6de04f71821d5136d9"
 end
 
-version "1.2.8" do
-  source md5: "44d667c142d7cda120332623eab69f40"
-end
-version "1.2.6" do
-  source md5: "618e944d7c7cd6521551e30b32322f4a"
-end
-
-source url: "https://zlib.net/zlib-#{version}.tar.gz",
+source url: "https://zlib.net/fossils/zlib-#{version}.tar.gz",
        extract: :seven_zip
 
 relative_path "zlib-#{version}"
